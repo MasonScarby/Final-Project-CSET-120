e04956fe72e590264c246045ed80cc3d790d697f
JtheGamer18 <150290900+JtheGamer18@users.noreply.github.com>
2023-11-30 12:36:00 -0500

    Update popup.js

--- Final Project/Feedback/popup.js
+++ Final Project/Feedback/popup.js
@@ -2,3 +2,14 @@ function togglePopup() {
     var popup = document.getElementById('popup');
     popup.classList.toggle('show-popup');
 }
+function showCongratsPopup() {
+    var congratsPopup = document.getElementById('congratsPopup');
+    congratsPopup.style.display = 'block';
+    setTimeout(function () {
+        hideCongratsPopup();
+    }, 3000);
+}
+function hideCongratsPopup() {
+    var congratsPopup = document.getElementById('congratsPopup');
+    congratsPopup.style.display = 'none';
+}
