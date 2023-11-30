7e0f57764db0d9c581807fd9a6541b273e701453
JtheGamer18 <150290900+JtheGamer18@users.noreply.github.com>
2023-11-30 12:42:06 -0500

    Update apps.js

--- Final Project/Login and Sign up/apps.js
+++ Final Project/Login and Sign up/apps.js
@@ -1,10 +1,33 @@
 document.getElementById("goToSignUp").addEventListener("click", function() {
-document.getElementById("container").style.transform = "translateX(-100%)";
+    document.querySelector('.sign-in-container').style.display = 'none';
+    document.querySelector('.sign-up-container').style.display = 'block';
+    document.querySelector('.manager-sign-up-container').style.display = 'none';
+    document.getElementById("container").style.transform = "translateX(-100%)";
 });
 
 document.getElementById("goToLogin").addEventListener("click", function() {
-document.getElementById("container").style.transform = "translateX(0)";
+    document.querySelector('.sign-in-container').style.display = 'block';
+    document.querySelector('.sign-up-container').style.display = 'none';
+    document.querySelector('.manager-sign-up-container').style.display = 'none';
+    document.getElementById("container").style.transform = "translateX(0)";
 });
+
+document.getElementById("goToManagerSignUp").addEventListener("click", function() {
+    document.querySelector('.manager-login-container').style.display = 'none';
+    document.querySelector('.sign-up-container').style.display = 'none';
+    document.querySelector('.manager-sign-up-container').style.display = 'block';
+    document.getElementById("container").style.transform = "translateX(-100%)";
+});
+
+document.getElementById("goToManagerLogin").addEventListener("click", function() {
+    document.querySelector('.manager-login-container').style.display = 'block';
+    document.querySelector('.sign-up-container').style.display = 'none';
+    document.querySelector('.manager-sign-up-container').style.display = 'none';
+    document.getElementById("container").style.transform = "translateX(0)";
+});
+
 function showManagerLogin() {
     document.querySelector('.manager-login-container').style.display = 'block';
-}
+    document.querySelector('.sign-in-container').style.display = 'none';
+    document.querySelector('.sign-up-container').style.display = 'none';
+    document.querySelector('.manager-sign-up-container').style.display = 'none';
